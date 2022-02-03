80ecbcb68f1d647dee299e10d3e81818b1c11d21
Jimebf <69jime@gmail.com>
2022-02-03 13:50:50 -0300

    Homework 01 Js

---
+++ 02-JS-I/homework/descripciones.js
@@ -0,0 +1,7 @@
+Variable: es una como una caja donde vamos a guardar datos para usar después
+Strings: es una palabra o texto que va a estar siempre entre comillas "" / ''
+Funciones: sirve para ejecutar una acción que se encuentre entre corchetes {}
+-argumentos: son las cosas que queremos que se cumplan ejecutando una funcion. Ej, (hola)
+-return: sirve para devolver algo que estaba previamente puesto
+Declaración if: esta sirve para colocar una condición (si x cosa es algo, debería devolver x cosa)
+Valores booleanos, en JS se usaran siempre True or false
