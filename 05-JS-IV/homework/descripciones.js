5e062b8708a66c4eeaddeabc314207f275e49013
Jimebf <69jime@gmail.com>
2022-02-10 02:17:10 -0300

    Homework 04 Js

---
+++ 05-JS-IV/homework/descripciones.js
@@ -0,0 +1,5 @@
+Objetos: estos van a tener mucha info sobre una sola cosa, se instancian usando {}
+Propiedades: es una asociacion entre un nombre (o clave) y un valor 
+Métodos: 
+Bucle for...in: 
+Notación de puntos vs notación de corchetes: 
