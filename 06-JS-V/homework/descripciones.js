d07e8eb144ad931beb5891f72b6bf9e973f522c3
Jimebf <69jime@gmail.com>
2022-02-11 14:20:45 -0300

    Homework 05 Js

---
+++ 06-JS-V/homework/descripciones.js
@@ -0,0 +1,2 @@
+Prototype: es una propiedad que despues podemos establecer en métodos.
+Constructors (de Clases): El método constructor es un metodo especial para crear e inicializar un objeto creado a partir de una clase.
